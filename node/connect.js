ae371186d77d9f7c063e1df035e85c7e7b21e586
Chris Cho <chris.cho@10gen.com>
2019-07-29 13:51:32 -0400

    sample files for node connection

---
+++ node/connect.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+
+// Read the certificates
+const cert = fs.readFileSync(__dirname + "/client.pem");
+const key = fs.readFileSync(__dirname + "/client.pem");
+const ca = fs.readFileSync(__dirname + "/ca.pem");
+
+// Customized for sample client keys
+const userName = "CN=Chris,OU=TestClientCertificateOrgUnit,O=EducationClientCertificate,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US";
+
+const client = new MongoClient(`mongodb://${encodeURIComponent(userName)}@localmongo1:27017?authMechanism=MONGODB-X509&ssl=true`, {
+  useNewUrlParser: true,
+  sslCA: ca,
+  sslKey:key,
+  sslCert:cert,
+});
+
+client.connect(function(err, db) {
+
+  var dbo = db.db("test");
+
+  dbo.collection('node-data').insertMany([
+    {
+      item: 'pizza',
+      qty: 25,
+      tags: ['pepperoni', 'tomato', 'dough'],
+    },
+    {
+      item: 'shortrib',
+      qty: 85,
+      tags: ['beef'],
+    }
+  ], function(err, res) {
+    if (err) throw err;
+    console.log("docs successfully inserted into node-data");
+    db.close();
+  });
+
+
+  client.close();
+});
