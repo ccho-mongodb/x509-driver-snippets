ae7b6d482224a1c87b8292fbb94e8dff3df9b908
Chris Cho <chris.cho@10gen.com>
2019-11-01 18:01:10 -0400

    add ssl_key to ruby driver example

--- ruby/connect.rb
+++ ruby/connect.rb
@@ -1,11 +1,14 @@
 require 'mongo'
 
 begin
-  client = Mongo::Client.new([ 'localmongo1:27017' ],
+  client = Mongo::Client.new( 
+                             ['localmongo1:27017'],
                              auth_mech: :mongodb_x509,
                              ssl: true,
-                             ssl_cert:    '/Users/ccho/dev/drivers/certs/v3/test-client.pem',
-                             ssl_ca_cert: '/Users/ccho/dev/drivers/certs/v3/test-ca.pem',
+                             ssl_cert:    '/Users/ccho/dev/x509-driver-snippets/certs/test-client.pem',
+                             ssl_key:    '/Users/ccho/dev/x509-driver-snippets/certs/test-client.pem',
+                             ssl_ca_cert: '/Users/ccho/dev/x509-driver-snippets/certs/test-ca.pem',
+
                             )
                              #ssl_verify: false,
                              #tlsInsecure: true)
